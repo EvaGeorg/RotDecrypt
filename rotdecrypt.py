7c102883de25acbdd9a0730c2ffbb8f856c9b1d9
EvaGeorg <eva.georgieva@hotmail.com>
2020-05-12 22:35:59 +0200

    Add files via upload

---
+++ rotdecrypt.py
@@ -0,0 +1,22 @@
+import string
+import sys
+
+file=sys.argv[1]
+
+def map_char(x):
+  pos = ((string.ascii_lowercase.index(x)+i)%26)
+  return string.ascii_lowercase[pos]
+
+for i in range(1,27):
+  f = open(file, "r")
+  while (1):
+    char = f.read(1)
+
+    char = char.lower()
+    if not char:
+      break
+    if (char.isalpha()):
+      encrypted = map_char(char)
+      open("decrypted"+str(i)+".txt", "a").write(encrypted)
+    else:
+      open("decrypted"+str(i)+".txt", "a").write(char)
